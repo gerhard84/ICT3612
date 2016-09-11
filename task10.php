a6ec6acbfda05adac24e4498d3dc91fda881dad3
gerhard84 <gerhard.g84@gmail.com>
2016-09-11 15:00:11 +0200

    task 10 - email missing

--- task10.php
+++ task10.php
@@ -5,6 +5,10 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <title>Task 10</title>
 </head>
+
+
+
+
 <body>
 <?php include_once("analyticstracking.php") ?>
 <div class="menu">
@@ -14,17 +18,43 @@
 
 <div class="container">
   <!--////////////////////////////// Task 10 (a)  //////////////////////////-->
+
+  <div class="row">
+    <div class="col-md-6">
+      <div class="panel panel-default">
+          <div class="panel-heading">
+              <h3 class="panel-title">Task 10 -  Display folder contents</h3>
+          </div>
+          <div class="panel-body">
+          <?php
+            $dir = opendir('txt/task10/');
+            while ($file = readdir($dir)) {
+              if ($file == '.' || $file == '..') {
+                continue;
+              }
+              echo $file . "</br>";
+            }
+            closedir($dir);
+          ?>
+          </div>
+      </div>
+    </div>
+  </div>
+
 	<div class="row">
 		<div class="col-md-6">
 			<div class="panel panel-default">
 			    <div class="panel-heading">
-			        <h3 class="panel-title">Task 10 (a) -  File Management Form</h3>
+			        <h3 class="panel-title">Task 10 -  Upload File</h3>
 			    </div>
 			    <div class="panel-body">
-
-
-
-
+            <h5> Please select a <strong>.txt</strong> file to upload:</h5>
+          <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="fileToUpload">
+            </br>
+            <input type="submit" value="Upload">
+          </form>
+        </br>
 
 
 
@@ -32,6 +62,42 @@
 			</div>
 		</div>
 	</div>
+
+
+  <div class="row">
+		<div class="col-md-6">
+			<div class="panel panel-default">
+			    <div class="panel-heading">
+			        <h3 class="panel-title">Task 10 -  Display file content</h3>
+			    </div>
+			    <div class="panel-body">
+
+
+          <?php
+          $ctime = 0;
+          foreach (glob('txt/task10/*.txt') as $file)
+          {
+          if ($ctime > filectime($file))
+          {
+          $match = $file;
+          $ctime = filectime($file);
+          }
+          }
+          echo "<strong>The contents of the file is:</strong></br>";
+          $latestFile = fopen("$file", "r") or die("Please upload a file");
+          while (!feof($latestFile)) {
+            $line = fgets($latestFile);
+            echo "$line</br>";
+          }
+
+          fclose($latestFile);          
+          ?>
+
+			    </div>
+			</div>
+		</div>
+	</div>
+
 
 
 
---
+++ upload.php
@@ -0,0 +1,49 @@
+<?php
+$target_dir = "txt/task10/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$uploadError = "Error";
+$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+if(isset($_POST["submit"])) {
+        $uploadOk = 1;
+      }
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    $uploadOk = 0;
+    $uploadError = "Sorry, file already exists.";
+
+
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 100000) {
+    $uploadOk = 0;
+    $uploadError = "Sorry, your file is too large.";
+
+
+}
+// Only Allow TXT files
+if($fileType != "txt" ) {
+    $uploadOk = 0;
+    $uploadError = "Sorry, only TXT files are allowed.";
+
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo $uploadError;
+
+
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      
+        echo "<meta http-equiv=\"refresh\" content=\"0;URL=task10.php\">";
+    } else {
+        $uploadError = "Sorry, there was an error uploading your file.";
+
+
+
+    }
+}
+?>
